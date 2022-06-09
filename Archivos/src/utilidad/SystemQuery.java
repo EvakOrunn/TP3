fe7b59d7565bef9651bcb59439f0abf573002dd3
EvakOrunn <gitLuisCr@gmail.com>
2022-06-09 12:02:14 -0300

    Se agregan nuevas clases y se corrige error al ejecutar el proyecto

---
+++ Archivos/src/utilidad/SystemQuery.java
@@ -0,0 +1,8 @@
+package utilidad;
+
+public class SystemQuery{
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
+    private static String OSArch = System.getProperty("os.arch");
+    
+}
